53c6a6c08bc6c1634dc017a0a33fc0b8ce68513d
Hyunsu Lee <hyunspookey@gmail.com>
2026-03-24 07:03:58 +0900

    make uv compatible

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -22,17 +22,10 @@ _____________________________________________________________________________
     :::::::  '''''''''''   ''''''''''''':::. -'\  \       C. SWANSIGER
 _____':::::_____________________________________\__\_________________________
 
-If you have not done so already, create a conda environment for your new 
-project with:
+If you have not done so already, install your new project with:
 
 cd {{cookiecutter.repo_name}}
-conda create --name {{cookiecutter.repo_name}} python=3.11
-conda activate {{cookiecutter.repo_name}}
-conda env export > environment.yml
-
-Install your new project in your local conda environment with:
-
-pip install -e .
+uv sync
 
 You will need to manually add data to .gitignore to prevent it from syncing to
 version control.
